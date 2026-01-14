f85f75d65dcc2868996b6450bdffe61352e6a015
sanket <sanketyeul@gmail.com>
2026-01-15 01:59:35 +0530

    'SECOND COM'MIT""

--- WEB_DESIGNE/src/App.jsx
+++ WEB_DESIGNE/src/App.jsx
@@ -1,15 +1,27 @@
-<div className="relative min-h-screen bg-black text-white overflow-x-hidden">
-  <Spotlight /> {/* cursor spotlight */}
+// src/App.jsx
+import React from 'react';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import HeroSection from './components/home/HeroSection';
+import FeaturesSection from './components/home/FeaturesSection';
+import StatsSection from './components/home/StatsSection';
+import AboutSection from './components/home/AboutSection';
+import AdvantagesSection from './components/home/AdvantagesSection';
 
-  <div className="relative z-10">
-    <Header />
-    <main>
-      <HeroSection />
-      <FeaturesSection />
-      <StatsSection />
-      <AboutSection />
-      <AdvantagesSection />
-    </main>
-    <Footer />
-  </div>
-</div>
+function App() {
+  return (
+    <div className="min-h-screen bg-black">
+      <Header />
+      <main>
+        <HeroSection />
+        <FeaturesSection />
+        <StatsSection />
+        <AboutSection />
+        <AdvantagesSection />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
